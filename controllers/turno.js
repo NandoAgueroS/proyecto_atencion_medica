5429ffe32c5dd335ce987b66ce814202d4314226
Nando Javier Agüero <nandoaguerojs@gmail.com>
2024-11-02 17:35:33 -0300

    vista de agenda creada

--- controllers/turno.js
+++ controllers/turno.js
@@ -4,5 +4,7 @@ exports.listar = async (req, res) => {
     const matricula = req.query.matricula || "M12345";
     const fecha = req.query.fecha || "2024-11-10";
     console.log(matricula, fecha);
-    res.send(await turno.get(matricula, fecha));
+    const turnos = await turno.get(matricula, fecha);
+    // res.send(await turno.get(matricula, fecha));
+    res.render('agenda/agenda', {turnos: turnos});
 }
