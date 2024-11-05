b73d36da4a96643569b9c95b1d4a25f7a6bbca41
Nando Javier Agüero <nandoaguerojs@gmail.com>
2024-11-05 18:48:18 -0300

    consultas desplegables y formato de fecha-hora

--- controllers/consulta.js
+++ controllers/consulta.js
@@ -68,6 +68,11 @@ exports.historiaClinica = async (req, res) => {
     // console.log(consultasDelPaciente);
     for await (const element of consultasDelPaciente){
         console.log(element);
+        try {
+            element.fecha = formatearFechaYHora(element.fecha);
+        } catch (error) {
+            console.log(error);
+        }
         hce.push({
             id_consulta: element.id_consulta,
             fecha_consulta: element.fecha,
@@ -88,4 +93,14 @@ exports.historiaClinica = async (req, res) => {
 
 exports.estadosDeDiagnosticos = async (req, res) => {
     res.json(await consulta.getEstadosDeDiagnosticos());
+}
+
+function formatearFechaYHora(fecha) {
+    const day = String(fecha.getDate()).padStart(2, '0');
+    const month = String(fecha.getMonth() + 1).padStart(2, '0');
+    const year = fecha.getFullYear();
+    const hour = String(fecha.getHours()).padStart(2, '0');
+    const minute = String(fecha.getMinutes()).padStart(2, '0');
+    const second = String(fecha.getSeconds()).padStart(2, '0');
+    return `${day}-${month}-${year} ${hour}:${minute}:${second}`;
 }
